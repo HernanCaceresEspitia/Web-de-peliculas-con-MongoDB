283eca3fb7c71693f9399d8e51f6a926b517bb31
HernanD <projectmgrex@hotmail.com>
2024-04-12 12:21:22 -0500

    Mejora de la lógica. Adicion de botones para los géneros y mejora de estilos

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,40 +1,30 @@
-
-const cards = document.getElementById("cards");
-
 const createCard = (arrayMovie = []) => {
-    const arrayCardMovie = arrayMovie.map((movie) => {
+    arrayMovie.forEach((movie) => {
         
-        //? Elementos básicos de la tarjeta
-
-        const cardMovie = document.createElement("div");
-        const nameMovie = document.createElement("h5");
-        const imgsMovie = document.createElement("img");
-        const buttonDel = document.createElement("button");
-
-        //? Contenedores para estilos
-
+        // Elementos básicos de la tarjeta
         const div13 = document.createElement("div");
         const singleTitle = document.createElement("div");
+        const nameMovie = document.createElement("h5");
         const imgContainer = document.createElement("div");
+        const imgsMovie = document.createElement("img");
         const dataMovie = document.createElement("div");
-        const infoSection1 = document.createElement("div")
+        const infoSection1 = document.createElement("div");
         const preTitle1 = document.createElement("div");
         const subTitle1 = document.createElement("div");
-        const infoSection2 = document.createElement("div")
+        const infoSection2 = document.createElement("div");
         const preTitle2 = document.createElement("div");
         const subTitle2 = document.createElement("div");
-        const infoSection3 = document.createElement("div")
+        const infoSection3 = document.createElement("div");
         const preTitle3 = document.createElement("div");
         const subTitle3 = document.createElement("div");
-        const infoSection4 = document.createElement("div")
+        const infoSection4 = document.createElement("div");
         const preTitle4 = document.createElement("div");
         const subTitle4 = document.createElement("div");
+        const genreContainer = document.createElement("div");
 
-        //? Añadir clases a los elementos
-
+        // Añadir clases a los elementos
         div13.classList.add("div13");
         singleTitle.classList.add("singleTitle");
-        singleTitle.setAttribute("id", "nombrePeli");
         imgContainer.classList.add("imgContainer");
         imgContainer.classList.add("card-img-top");
         imgsMovie.classList.add("imgEditor");
@@ -50,13 +40,7 @@ const createCard = (arrayMovie = []) => {
         subTitle3.classList.add("subTitle");
         infoSection4.classList.add("infoSection");
         preTitle4.classList.add("preTitle");
-        subTitle4.classList.add("subTitle");
-        
-
-        //? Asignacion de los valores a los elementos del documento
-
-        buttonDel.innerText = "X";
-        //*Agregar la asignacion de eliminación
+        genreContainer.classList.add("genreContainer"); // Añadir clase al contenedor de géneros
         nameMovie.innerText = movie.title;
         imgsMovie.src = movie.poster;
         preTitle1.innerText = "Director: ";
@@ -67,32 +51,33 @@ const createCard = (arrayMovie = []) => {
         subTitle1.innerText = movie.director;
         subTitle2.innerText = movie.year;
         subTitle3.innerText = movie.duration;
-        subTitle4.innerText = movie.genre;
-        
-        
-        //? Append para ubicar los elementos
 
-
-
-        infoSection1.append(preTitle1, subTitle1)
+        // Append para ubicar los elementos
+        infoSection1.append(preTitle1, subTitle1);
         infoSection2.append(preTitle2, subTitle2);
         infoSection3.append(preTitle3, subTitle3);
-        infoSection4.append(preTitle4, subTitle4);
+        infoSection4.append(preTitle4, genreContainer);
         dataMovie.append(infoSection1, infoSection2, infoSection3, infoSection4);
         imgContainer.append(imgsMovie);
         singleTitle.append(nameMovie);
-        div13.append(singleTitle, imgContainer, dataMovie);
-        cards.append(div13);
-        
+        div13.append(singleTitle, imgContainer, dataMovie); // Agregar el contenedor de géneros a la tarjeta
 
-        div13.addEventListener("mouseover", () => {
-            const tituloPelicula = movie.title;
-            document.getElementById("tituloPelicula").innerText = tituloPelicula;
+        cards.appendChild(div13);
+
+        // Obtener los datos del array 'genre' y crear botones para cada género
+        movie.genre.forEach((genre) => {
+            const button = document.createElement("button");
+            button.innerText = genre;
+            button.classList.add("custom-button");
+            genreContainer.appendChild(button);
         });
 
-    })
+        // Cambiar el título por el de la película en vista
+        div13.addEventListener("mouseover", () => {
+            document.getElementById("tituloPelicula").innerText = movie.title;
+        });
+    });
 };
-
 
 const getDataAndCreateCard = () => {
     $.get("https://students-api.up.railway.app/movies", (data, status) => {
@@ -100,7 +85,4 @@ const getDataAndCreateCard = () => {
     });
 };
 
-
-
 getDataAndCreateCard();
-
