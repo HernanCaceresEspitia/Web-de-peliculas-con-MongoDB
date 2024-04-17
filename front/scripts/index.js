07be4b1086ae8e10972a5b53857f6a1cfb0bd026
HernanD <projectmgrex@hotmail.com>
2024-04-16 19:21:21 -0500

    Implementación de AXIOS

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,9 +1,14 @@
-const createCard = require("/scripts/createCard")
+const axios = require("axios");
+const createCard = require("./createCard");
 
-const getDataAndCreateCard = () => {
-    $.get("https://students-api.up.railway.app/movies", (data, status) => {
-        createCard(data); // Llama a createCard con los datos de la API
-    });
+const getDataAndCreateCard = async () => {
+    try {
+        const response = await axios.get("https://students-api.up.railway.app/movies");
+        createCard(response.data); // Llama a createCard con los datos de la API
+    } catch (error) {
+        console.error("Error al obtener los datos de películas:", error);
+    }
 };
+
 
 getDataAndCreateCard();
